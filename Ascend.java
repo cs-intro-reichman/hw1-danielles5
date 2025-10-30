b51361d56b2ffba5cab5938749594f77016333a3
Danielle Sinay <danielle.sinay@post.runi.ac.il>
2025-10-31 00:13:41 +0200

    Completed Ascend Assignment

--- Ascend.java
+++ Ascend.java
@@ -1,7 +1,45 @@
-// Generates three integer random numbers in a given range,
-// and prints them in increasing order.
+
 public class Ascend {
 	public static void main(String[] args) {
-		// Replace this comment with your code
+		
+		// Must give the minimum of the range and then the maximum of the range as input!
+		int lowerRange = Integer.parseInt(args[0]);
+		int upperRange = Integer.parseInt(args[1]);
+
+		int rangeSize = upperRange - lowerRange + 1;
+
+		// Generates three integer random numbers in a given range,
+		int randomOne = (int)(Math.random() * rangeSize ) + lowerRange;
+		int randomTwo = (int)(Math.random() * rangeSize ) + lowerRange;
+		int randomThree = (int)(Math.random() * rangeSize ) + lowerRange;
+
+		// Puts the three integers in increasing order and prints
+		int holder;
+
+		if (randomOne > randomTwo) {
+			
+			holder = randomTwo;
+			randomTwo = randomOne;
+			randomOne = holder;
+		} 
+		
+		if (randomTwo > randomThree) {
+
+			holder = randomThree;
+			randomThree = randomTwo;
+			randomTwo = holder;
+				
+		} 
+		
+		if (randomOne > randomTwo) {
+			
+			holder = randomTwo;
+			randomTwo = randomOne;
+			randomOne = holder;
+
+		}
+
+		System.err.println(randomOne + " " + randomTwo + " " + randomThree);
+
 	}
 }
