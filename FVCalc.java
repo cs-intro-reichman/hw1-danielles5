935f99b7d780564add2a1c83f7ab4a33f2c5e16a
Danielle Sinay <danielle.sinay@post.runi.ac.il>
2025-10-31 01:09:11 +0200

    Completed FVCalc Assignment - check if compound interest equation was correct

--- FVCalc.java
+++ FVCalc.java
@@ -1,6 +1,15 @@
 // Computes the future value of a saving investment.
 public class FVCalc {
 	public static void main(String[] args){
-		// Replace this comment with your code
+		// Recieves the amount of savings, the average return rate, and amount of time (in years) planned to save
+		double savings = Double.parseDouble(args[0]);
+		double returnRate = Double.parseDouble(args[1]);
+		double years = Double.parseDouble(args[2]);
+
+		// Calculates compound interest and prints
+		double futureValue = savings * Math.pow(1 + returnRate , years);
+
+		System.err.println("If you invest $" + savings + " today at a " + returnRate + 
+							"% rate, you will have approx. $" + futureValue + " in " + years + " years.");
 	}
 }
