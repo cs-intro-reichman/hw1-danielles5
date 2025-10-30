1996845d75d07e755622cdab35e4e28b6e1af670
Danielle Sinay <danielle.sinay@post.runi.ac.il>
2025-10-31 00:37:36 +0200

    Completed Bill3 Assignment - ask about style

--- Bill3.java
+++ Bill3.java
@@ -1,8 +1,18 @@
 // Splits a restaurant bill evenly among three diners.
 public class Bill3 {
 	public static void main(String[] args) {
-		// To get you started, here is the first line in the program:
+		// Input recieved as 3 names (of the diners) and their total bill (as the final input)
 	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+		String name2 = args[1];
+		String name3 = args[2];
+		double billTotal = Double.parseDouble(args[3]);
+
+		// Calculates the total each person should pay, and prints it
+		double splitBill = billTotal / 3;
+		int roundedBill = (int)(splitBill + 1);
+
+		System.err.println("\n" + name1 + " should pay: $" + splitBill + "\nRounded up: $" + roundedBill + ".00\n" +
+							"\n" + name2 + " should pay: $" + splitBill + "\nRounded up: $" + roundedBill + ".00\n" +
+							"\n" + name3 + " should pay: $" + splitBill + "\nRounded up: $" + roundedBill + ".00\n");
 	}
 }
