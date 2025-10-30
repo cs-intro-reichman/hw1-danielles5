a622af2c3559c5d1a0fb95056277626590649c26
Danielle Sinay <danielle.sinay@post.runi.ac.il>
2025-10-31 00:51:16 +0200

    Completed PrintTo5 Assignment

--- PrintTo5.java
+++ PrintTo5.java
@@ -5,7 +5,7 @@ public class PrintTo5 {
 		// Declares an int variable, and sets it to 1
         int i = 0;
         // Complete the program's code here:
-        while (i<=5) {
+        while (i <= 5) {
             System.err.println(i);
             i++;
         }
