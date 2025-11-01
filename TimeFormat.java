23a724e950482304fb2db930d52465236cf0e2a4
Danielle Sinay <danielle.sinay@post.runi.ac.il>
2025-11-01 19:33:55 +0200

    Completed TimeFormat Assignment, ask about int vs string

--- TimeFormat.java
+++ TimeFormat.java
@@ -1,17 +1,41 @@
 // Represents the hh:mm time format using an AM/PM format. 
 public class TimeFormat {
 	public static void main(String[] args) {
-		// In Java, the command-line arguments args[0], args[1], ... are represented
-		// each by a string. In this program, the single "hh:mm" input is represented
+		// In this program, the single "hh:mm" input is represented
 		// by the single command-line string argument args[0]. 
-		//   
-		// The following statement handles the hours part of the input.
-		// It concatenates the empty string "" with the leftmost hour-digit. 
+
+		// Concatenates the empty string "" with the leftmost hour-digit. 
 		// It then concatenates the resulting string with the rightmost hour-digit,
 		// and then uses parseInt to cast the resulting string as an int.
 		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
 		// Does the same with the minutes part of the input.
-		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
-        // Replace this comment with the rest of your code
+		//int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
+
+
+		// Takes the minutes input as a String
+		
+        String minutes = "" + args[0].charAt(3) + args[0].charAt(4);
+		
+		
+
+		// Checks if the military time is AM or PM and prints accordingly
+		String morningNight;
+
+		if (hours < 12) {
+			morningNight = "AM";
+			System.err.println(hours + ":" + minutes + " " + morningNight);
+
+		} else if (hours == 12) {
+			morningNight = "PM";
+			System.err.println(hours + ":" + minutes + " " + morningNight);
+
+		} else {
+			morningNight = "PM";
+			hours = hours - 12;
+			System.err.println(hours + ":" + minutes + " " + morningNight);
+
+		}
+
+
 	}
 }
