3a724fa1507d23047b87adff1940511cbd52261b
Danielle Sinay <danielle.sinay@post.runi.ac.il>
2025-11-01 18:29:14 +0200

    Completed Bill3 based off of reply style of grader, ask about Math.ceil

--- Bill3.java
+++ Bill3.java
@@ -1,7 +1,7 @@
 // Splits a restaurant bill evenly among three diners.
 public class Bill3 {
 	public static void main(String[] args) {
-		// Input recieved as 3 names (of the diners) and their total bill (as the final input)
+		// Input received as 3 names (of the diners) and their total bill (as the final input)
 	    String name1 = args[0];
 		String name2 = args[1];
 		String name3 = args[2];
@@ -9,10 +9,17 @@ public class Bill3 {
 
 		// Calculates the total each person should pay, and prints it
 		double splitBill = billTotal / 3;
-		int roundedBill = (int)(splitBill + 1);
+		double billRounded = Math.ceil(splitBill);
+		/* 
+		if (splitBill % 1 == 0) {
+			int billRounded = (int)splitBill;
+			System.err.println("Dear " + name3 + ", " + name2 + ", and " + name1 + ": please pay " + billRounded + " shekels each.");
+		} else {
+			int billRounded = (int)(splitBill + 1);
+			System.err.println("Dear " + name3 + ", " + name2 + ", and " + name1 + ": please pay " + billRounded + " shekels each.");
+		}
+		*/
 
-		System.err.println("\n" + name1 + " should pay: $" + splitBill + "\nRounded up: $" + roundedBill + ".00\n" +
-							"\n" + name2 + " should pay: $" + splitBill + "\nRounded up: $" + roundedBill + ".00\n" +
-							"\n" + name3 + " should pay: $" + splitBill + "\nRounded up: $" + roundedBill + ".00\n");
+		System.err.println("Dear " + name3 + ", " + name2 + ", and " + name1 + ": pay " + billRounded + " Shekels each");
 	}
 }
